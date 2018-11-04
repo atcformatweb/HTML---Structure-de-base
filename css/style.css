74920e2854ef0a40aebca6cc381b6ce566e42236
Thomas Gravy <thomas.gravy@promsocatc.net>
2018-11-04 21:14:42 +0100

    Initial commit

---
+++ css/style.css
@@ -0,0 +1,96 @@
+<!DOCTYPE html> 
+    <!--  
+        n'est pas une balise HTML, 
+        c'est une instruction au navigateur Web 
+        concernant la version de HTML dans laquelle 
+        la page est écrite.
+    -->
+
+    <head>
+        <title>HTML</title>
+        <!--    
+                définit un titre dans la barre d'outils du navigateur
+                fournit un titre à la page lorsqu'elle est ajoutée aux favoris
+                affiche un titre pour la page dans les résultats du moteur de recherche 
+        -->
+
+        <meta charset="UTF-8">
+         <!--
+             spécifie le codage des caractères du document HTML.
+         -->
+
+        <meta name="description" content="...">
+
+        <meta name="keywords" content="..., ..., ..., ...">
+
+        <meta name="author" content="...">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--
+            viewport donne au navigateur des instructions sur la façon de contrôler les 
+            dimensions et la mise à l'échelle de la page.
+        -->
+
+        <base href="https://www.MonSite.com/images/" target="_blank">
+        <!--
+             spécifie l'URL / la cible de base pour toutes les URL relatives d'un document.
+        -->
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+        <style>
+            .demo{
+                Color:green;
+                font-size:20px;
+            }
+        </style>
+
+
+        <script src="myscripts.js"></script>
+        <script type="text/javascript">
+            document.getElementById("demo").innerHTML = "Hello JavaScript!";
+        </script>
+
+        <noscript>Your browser does not support JavaScript!</noscript>
+        <!--
+            définit un autre contenu pour les utilisateurs ayant 
+            désactivé les scripts dans leur navigateur ou disposant 
+            d'un navigateur ne prenant pas en charge les scripts.
+        -->
+    </head>
+    <body>
+
+        <h1>Titres</h1>
+        <p id="information" class="paragraphe" >All meta information goes in the head section...</p>
+
+        <img src="image.jpg" width="24" height="39" alt="image" />
+        
+        <br/><br/>
+
+        <a href="https://www.w3schools.com/" title="Lien vers w3schools" >w3schools</a>
+
+        <hr />
+
+        <div id="demo"></div>
+
+
+
+
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+        <style>
+            .demo{
+                Color:green;
+                font-size:20px;
+            }
+        </style>
+
+        <script src="myscripts.js"></script>
+        <script type="text/javascript">
+            document.getElementById("demo").innerHTML = "Hello JavaScript!";
+        </script>
+
+        <noscript>Your browser does not support JavaScript!</noscript>
+    </body>
+</html>
